ea7730895c7006ae062b761b714e6b12421caa2a
Md Noornawaz Rahman <noornawazrahman@gmail.com>
2026-01-04 23:12:11 +0530

    feat(db): add PostgreSQL support with Docker and config

--- src/main/java/com/mnrahman/ProjectManagementSystemApplication.java
+++ src/main/java/com/mnrahman/ProjectManagementSystemApplication.java
@@ -1,10 +1,20 @@
 package com.mnrahman;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProjectManagementSystemApplication {
+
+
+//	@PostConstruct
+//	public void init() {
+//		// Set JVM timezone to UTC before anything else happens
+//		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementSystemApplication.class, args);
